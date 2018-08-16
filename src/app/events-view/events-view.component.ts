52e4f9244a0a0e07ccfdf7a473c959054133ce47
victor <vgarzom@gmail.com>
2018-08-15 20:11:03 -0500

    Permite crear eventos

--- src/app/events-view/events-view.component.ts
+++ src/app/events-view/events-view.component.ts
@@ -9,8 +9,11 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventsViewComponent implements OnInit {
   private _open = false;
+  private _isShowingEventCreation = false;
   private user: any;
-  private events : any = [];
+  private events: any = [];
+  private newEvent: any = { durationType: "m" }
+  private canCreateEvent = false
 
   constructor(private router: Router, private http: HttpClient) { }
 
@@ -25,7 +28,7 @@ export class EventsViewComponent implements OnInit {
   }
 
   get open() {
-    return this._open
+    return this._open;
   }
 
   set open(value: boolean) {
@@ -33,6 +36,19 @@ export class EventsViewComponent implements OnInit {
     if (!value) {
       // Put your cleanup code here
       console.log("Cleaning up");
+    }
+  }
+
+  get isShowingEventCreation() {
+    return this._isShowingEventCreation;
+  }
+
+  set isShowingEventCreation(value: boolean) {
+    this._isShowingEventCreation = value;
+    if (!value) {
+      // Put your cleanup code here
+      console.log("Cleaning up");
+      this.newEvent = { durationType: "m" }
     }
   }
 
@@ -51,4 +67,34 @@ export class EventsViewComponent implements OnInit {
     })
   }
 
+  public onCreateEvent() {
+    if (this.newEvent.durationType === "h") {
+      this.newEvent.duration = this.newEvent.duration * 60;
+      this.newEvent.durationType = "m";
+    }
+    this.newEvent.user_id = this.user._id;
+
+    console.log(JSON.stringify(this.newEvent))
+    this.http.post('api/event', this.newEvent).subscribe(data => {
+      if (data["code"] != 200){
+        alert(data["message"])
+      }else {
+        this.events.push(data["event"])
+        this.canCreateEvent = false;
+        this.isShowingEventCreation = false;
+      }
+    })
+  }
+
+  public validateEventCreation() {
+    if (this.newEvent.name !== undefined && this.newEvent.name !== null && this.newEvent.name !== "" &&
+      this.newEvent.description !== undefined && this.newEvent.description !== null && this.newEvent.description !== "" &&
+      this.newEvent.event_date !== undefined && this.newEvent.event_date !== null && this.newEvent.event_date !== "" &&
+      this.newEvent.duration !== undefined && this.newEvent.duration !== null && this.newEvent.duration !== "") {
+
+      this.canCreateEvent = true;
+    } else {
+      this.canCreateEvent = false;
+    }
+  }
 }
